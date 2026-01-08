91c988efcf11a9a0fed2e18e2ed289c7a3833da6
Yong Su <jean.timex@gmail.com>
2026-01-07 17:21:31 -0800

    fix: add 01-hello-webgpu.html to vite build inputs

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
   base: "/webgpu-tutorial/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        "01-hello-webgpu": resolve(__dirname, "01-hello-webgpu.html"),
+      },
+    },
+  },
 });
