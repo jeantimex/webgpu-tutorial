4fa192acaaa31e96a43fce62426c9cd6a73bac14
Yong Su <jean.timex@gmail.com>
2026-01-07 17:22:42 -0800

    chore: use glob to dynamically configure vite build inputs

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,19 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
+import { globSync } from "glob";
 
 export default defineConfig({
   base: "/webgpu-tutorial/",
   build: {
     rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-        "01-hello-webgpu": resolve(__dirname, "01-hello-webgpu.html"),
-      },
+      input: Object.fromEntries(
+        globSync("*.html").map((file) => [
+          // Use the filename without extension as the entry name
+          // e.g., "01-hello-webgpu.html" -> "01-hello-webgpu"
+          file.slice(0, -5),
+          resolve(__dirname, file),
+        ])
+      ),
     },
   },
 });
